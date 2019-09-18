c9cf2cb8f6815921a6fa5b0d08ac15a003c00c4d
icydwei <robpaik@gmail.com>
2019-09-18 18:01:54 -0400

    challenge 3 complete, working on challenge 4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,56 @@
-console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/6";
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+document.addEventListener("DOMContentLoaded", function(){
+
+
+  loadImages()
+  loadBreeds()
+  filterBreeds()
+
+
+  })
+
+
+
+function loadImages(){
+  const imgCont = document.getElementById("dog-image-container");
+
+  fetch(imgUrl)
+  .then(response => response.json())
+  .then(imageCollection => {
+    imageCollection.message.forEach(function (image) {
+      var imgEle = document.createElement("IMG")
+      imgEle.src = image;
+      imgEle.height = 250;
+      imgEle.width = 250;
+      imgEle.border = 5;
+      imgCont.appendChild(imgEle)
+    })
+  }
+)}
+
+function loadBreeds() {
+  const breedList = document.getElementById("dog-breeds");
+  fetch(breedUrl)
+  .then(response => response.json())
+  .then(breedCollection => {
+    Object.keys(breedCollection.message).forEach(function(breed) {
+      var liTag = document.createElement("li")
+      liTag.innerText = breed.charAt(0).toUpperCase() + breed.slice(1,-1)
+      liTag.addEventListener("click", () => {
+        liTag.style.color = "purple";
+        liTag.style.fontWeight = "bold";
+      })
+      breedList.appendChild(liTag)
+    })
+  })
+}
+
+function filterBreeds() {
+  const letterSelect = document.getElementById("breed-dropdown");
+  letterSelect.addEventListener("change", (e) => {
+    console.log("change", e)
+  })
+}
